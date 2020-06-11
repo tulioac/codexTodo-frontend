6177f7ef6769dafe6609f70bb02716c4614e8dca
Tulio <tutex1500@gmail.com>
2020-06-11 20:31:02 -0300

    Estilo login

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,8 +1,15 @@
 import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 
 export default props =>
   <Fragment>
+    <h2>Login</h2>
     <input type="email" name="email" placeholder="Email" className="caixa" />
     <input type="password" name="senha" placeholder="Senha" className="caixa" />
     <button>Acessar</button>
+
+    <div className="extra">
+      <p>Não possui conta?</p>
+      <Link to='/register'>Cadastre-se</Link>
+    </div>
   </Fragment >
