096638a0646fb20fb59d2f2fc89d376a37912e5f
Tulio <tutex1500@gmail.com>
2020-06-12 12:12:21 -0300

    Estilo dos forms de login e cadastro

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,15 +1,18 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default props =>
-  <Fragment>
+  <div>
     <h2>Login</h2>
-    <input type="email" name="email" placeholder="Email" className="caixa" />
-    <input type="password" name="senha" placeholder="Senha" className="caixa" />
-    <button>Acessar</button>
+
+    <form action="">
+      <input type="email" name="email" placeholder="Email" className="caixa" />
+      <input type="password" name="senha" placeholder="Senha" className="caixa" />
+      <button>Acessar</button>
+    </form>
 
     <div className="extra">
       <p>Não possui conta?</p>
       <Link to='/register'>Cadastre-se</Link>
     </div>
-  </Fragment >
+  </div >
--- src/components/register/index.jsx
+++ src/components/register/index.jsx
@@ -1,3 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default props => <h1>Register</h1>
+export default props =>
+  <div id="register">
+    <h2>Cadastro</h2>
+
+    <form action="">
+      <input type="text" name="nome" placeholder="Nome" className="caixa" />
+      <input type="email" name="email" placeholder="Email" className="caixa" />
+      <input type="password" name="senha" placeholder="Senha" className="caixa" />
+      <button>Criar Conta</button>
+    </form>
+
+    <div className="extra">
+      <p>Já possui conta?</p>
+      <Link to='/login'>Acesse-a</Link>
+    </div>
+  </div >
