c3db8419a66465ac3898af4a8d128e83d97910a3
Tulio <tutex1500@gmail.com>
2020-06-15 10:24:11 -0300

    Estilo do input de Nova Tarefa

--- src/components/addTask/index.jsx
+++ src/components/addTask/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './styles.css';
-import plus from '../../svg/plus.svg';
-import plusFilled from '../../svg/plusFilled.svg';
 
 export default props => {
-  return <div className="center">
-    <img src={plus} />
-    <input type="text" placeholder="Adicionar tarefa" className="textLike"></input>
+  return <div id="addTask">
+    <input type="text" placeholder="Nova tarefa" className="textLike"></input>
+    <button className="vazio">
+      Criar
+    </button>
   </div>
 }
