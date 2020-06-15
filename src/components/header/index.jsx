67b60171bfffc98ee42cd6872dcc6266f8de55f4
Tulio <tutex1500@gmail.com>
2020-06-15 13:30:40 -0300

    Remoção dos logs

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -6,12 +6,7 @@ import { Link } from 'react-router-dom';
 export default class Header extends Component {
 
   logout = () => {
-    console.log('Saindo da conta...');
-
     sessionStorage.removeItem('token');
-
-    console.log('SessionStorage', sessionStorage.getItem('token'));
-
     this.props.setToken(null);
 
     // TODO: Enviar para o servidor o token invalidado
@@ -19,8 +14,6 @@ export default class Header extends Component {
 
   render() {
     const titulo = <h1>To<span className="sublinhado espesso">Do</span> List</h1>
-
-    console.log('Header autenticado', this.props.authenticated);
 
     const sair = this.props.authenticated ?
       <button className="vazio" id="userLogout" onClick={this.logout}>
